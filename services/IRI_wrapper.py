30f72ab1658d38e66a74933718ebdeffe2c5b98b
Nolan Gregory <ZNG3030@conagrafoods.com>
2024-03-14 09:44:55 -0500

    ermmmm

---
+++ services/IRI_wrapper.py
@@ -0,0 +1,7 @@
+"""
+Call container to perform the IRI data analysis
+
+The IRI data gets aggregated within the container, and the container deals with the process of 
+pivoting the data on flavors and forms. The container will return the JSON response from the dataframe
+that is created in the process. 
+"""
